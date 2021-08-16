8eea635558084c5a17fe76ce329eceeb9f7af668
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2021-08-16 18:32:33 +0530

    Add files via upload

---
+++ content/res/var/Funny/Funny_eng.js
@@ -0,0 +1,354 @@
+tmp = 0;
+
+tmp ++;
+window["w" + tmp] = "the sun shines when you are happy"; //Funny
+window["b" + tmp] = "it rains when you are sad";
+window["y" + tmp] = 78;
+window["n" + tmp] = 22;
+
+tmp ++;
+window["w" + tmp] = "you had a magic carpet"; 
+window["b" + tmp] = "you are not allowed to get off it";
+window["y" + tmp] = 29; 
+window["n" + tmp] = 71; 
+
+tmp ++;
+window["w" + tmp] = "you could have any one super hero's powers"; 
+window["b" + tmp] = "a super villian will always be looking to get you killed";
+window["y" + tmp] = 43;
+window["n" + tmp] = 57;
+
+tmp ++;
+window["w" + tmp] = "there was peace on Earth";
+window["b" + tmp] = "internet disappeared";
+window["y" + tmp] = 52;
+window["n" + tmp] = 48;
+
+tmp ++;
+window["w" + tmp] = "you point out five people that, along with yourself, would be terribly successful";
+window["b" + tmp] = "you also point out five people whose lives will be destroyed";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "you can never fail";
+window["b" + tmp] = "you can never give up";
+window["y" + tmp] = 80;
+window["n" + tmp] = 20;
+
+tmp ++;
+window["w" + tmp] = "you learned three new languages fluently";
+window["b" + tmp] = "you forget your native language";
+window["y" + tmp] = 39;
+window["n" + tmp] = 61;
+
+tmp ++;
+window["w" + tmp] = "you could live forever";
+window["b" + tmp] = "you may never talk to someone again";
+window["y" + tmp] = 19;
+window["n" + tmp] = 81;
+
+tmp ++;
+window["w" + tmp] = "you got a magic printer which could print real money";
+window["b" + tmp] = "you need to wear a kilt and no underwear for the rest of your life";
+window["y" + tmp] = 46;
+window["n" + tmp] = 54;
+
+tmp ++;
+window["w" + tmp] = "you have twice as great sex every time";
+window["b" + tmp] = "you have to have sex every day, or you will die";
+window["y" + tmp] = 53;
+window["n" + tmp] = 47;
+
+tmp ++;
+window["w" + tmp] = "the first thing you hear every morning is 'I Love You'";
+window["b" + tmp] = "it is always different people who say it";
+window["y" + tmp] = 37;
+window["n" + tmp] = 63;
+
+tmp ++;
+window["w" + tmp] = "you were always in good shape";
+window["b" + tmp] = "you have to exercise a few times a week";
+window["y" + tmp] = 85;
+window["n" + tmp] = 15;
+
+tmp ++;
+window["w" + tmp] = "you got a clone of yourself that obeys your orders";
+window["b" + tmp] = "it falls in love with the same person you fall in love with";
+window["y" + tmp] = 30;
+window["n" + tmp] = 70;
+
+tmp ++;
+window["w" + tmp] = "your sense of smell is that of a dog's";
+window["b" + tmp] = "you have bad smell";
+window["y" + tmp] = 21;
+window["n" + tmp] = 79;
+
+tmp ++;
+window["w" + tmp] = "you can turn icecream into gold";
+window["b" + tmp] = "you will become bald for the rest of your life";
+window["y" + tmp] = 32;
+window["n" + tmp] = 68;
+
+tmp ++;
+window["w" + tmp] = "there is always a surprise party arranged for you when you get home";
+window["b" + tmp] = "you hate food and dancing";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "you could marry any celebrity";
+window["b" + tmp] = "you are the ugliest person in the world";
+window["y" + tmp] = 18;
+window["n" + tmp] = 82;
+
+tmp ++;
+window["w" + tmp] = "you get more attractive by every day that passes";
+window["b" + tmp] = "you can only sleep at morning";
+window["y" + tmp] = 25;
+window["n" + tmp] = 75;
+
+tmp ++;
+window["w" + tmp] = "you could have any food at the snap of a finger";
+window["b" + tmp] = "but you hate chocolate and pizza";
+window["y" + tmp] = 44;
+window["n" + tmp] = 56;
+
+tmp ++;
+window["w" + tmp] = "your father wins $50,000 on a lottery ticket you gave him";
+window["b" + tmp] = "he won't share anything of it with you";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "you are rich and famous for the rest of your life";
+window["b" + tmp] = "you can't ride any vehicle except bicycle";
+window["y" + tmp] = 32;
+window["n" + tmp] = 68;
+
+tmp ++;
+window["w" + tmp] = "you get a new pair of expensive shoes";
+window["b" + tmp] = "you cant buy or use any other shoes";
+window["y" + tmp] = 12;
+window["n" + tmp] = 88;
+
+tmp ++;
+window["w" + tmp] = "you could use any super power for ten minutes";
+window["b" + tmp] = "your girlfriend will become too fat";
+window["y" + tmp] = 40;
+window["n" + tmp] = 60;
+
+tmp ++;
+window["w" + tmp] = "you could rotate your head 360 degree";
+window["b" + tmp] = "your lifespan will be half as your real lifespan";
+window["y" + tmp] = 14;
+window["n" + tmp] = 86;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tmp ++;
+window["w" + tmp] = "you could be as rich as you want";
+window["b" + tmp] = "you will become mad after the age of 30";
+window["y" + tmp] = 18;
+window["n" + tmp] = 82;
+
+tmp ++;
+window["w" + tmp] = "you could live a long, healthy, happy life with anyone you love";
+window["b" + tmp] = "you will be sick most of the time";
+window["y" + tmp] = 86;
+window["n" + tmp] = 14;
+
+tmp ++;
+window["w" + tmp] = "you get a chance to know everything about the universe";
+window["b" + tmp] = "you never experience emotions again";
+window["y" + tmp] = 32;
+window["n" + tmp] = 68;
+
+tmp ++;
+window["w" + tmp] = "you could bring someone or something back from the dead";
+window["b" + tmp] = "someone you love or someone close to you has to die";
+window["y" + tmp] = 11;
+window["n" + tmp] = 89;
+
+tmp ++;
+window["w" + tmp] = "you can kill your worst enemy";
+window["b" + tmp] = "you will be haunted by them";
+window["y" + tmp] = 14;
+window["n" + tmp] = 86;
+
+tmp ++;
+window["w" + tmp] = "every time you see a girl you can make her fall in love with you instantly with the superpower";
+window["b" + tmp] = "you and your life partner will be poor always";
+window["y" + tmp] = 30;
+window["n" + tmp] = 70;
+
+tmp ++;
+window["w" + tmp] = "you could fly and can breathe even on space";
+window["b" + tmp] = "you are the only living creature in the universe";
+window["y" + tmp] = 44;
+window["n" + tmp] = 56;
+
+tmp ++;
+window["w" + tmp] = "you could make any video game scene as real";
+window["b" + tmp] = "the villians will always come after you";
+window["y" + tmp] = 45;
+window["n" + tmp] = 55;
+
+tmp ++;
+window["w" + tmp] = "you could talk to any animal you see";
+window["b" + tmp] = "humans no longer understand you";
+window["y" + tmp] = 19;
+window["n" + tmp] = 81;
+
+tmp ++;
+window["w" + tmp] = "you could live in the world as your favorite book/movie";
+window["b" + tmp] = "you have to be villian";
+window["y" + tmp] = 48;
+window["n" + tmp] = 52;
+
+tmp ++;
+window["w" + tmp] = "you could have any 3 superpowers you wanted";
+window["b" + tmp] = "you had to sacrifice 3 of your friends for it";
+window["y" + tmp] = 34;
+window["n" + tmp] = 66;
+
+tmp ++;
+window["w" + tmp] = "you won the lottery";
+window["b" + tmp] = "your both leg should cut off";
+window["y" + tmp] = 31;
+window["n" + tmp] = 69;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 24;
+window["n" + tmp] = 76;
+
+fileLoaded = true; //do not remove this
---
+++ content/res/var/Funny/Funny_hin.js
@@ -0,0 +1,107 @@
+tmp = 0;
+
+tmp ++;
+window["w" + tmp] = "जब आप खुश होते हैं तो सूरज चमकता है"; //what_if
+window["b" + tmp] = "जब आप उदास होते हैं तो बारिश होती है"; //but
+window["y" + tmp] = 78; //yes
+window["n" + tmp] = 22; //no
+
+tmp ++;
+window["w" + tmp] = "आपके पास एक जादुई कालीन था"; 
+window["b" + tmp] = "आपको इससे बाहर निकलने की अनुमति नहीं है";
+window["y" + tmp] = 29; 
+window["n" + tmp] = 71; 
+
+tmp ++;
+window["w" + tmp] = "आपके पास किसी भी सुपरहीरो की शक्तियां हो सकती हैं"; 
+window["b" + tmp] = "एक सुपर विलेन हमेशा आपको मारना चाहता है";
+window["y" + tmp] = 43;
+window["n" + tmp] = 57;
+
+tmp ++;
+window["w" + tmp] = "पृथ्वी पर शांति थी";
+window["b" + tmp] = "इंटरनेट गायब हो गया";
+window["y" + tmp] = 52;
+window["n" + tmp] = 48;
+
+tmp ++;
+window["w" + tmp] = "you point out five people that, along with yourself, would be terribly successful";
+window["b" + tmp] = "you also point out five people whose lives will be destroyed";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "you can never fail";
+window["b" + tmp] = "you can never give up";
+window["y" + tmp] = 80;
+window["n" + tmp] = 20;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+tmp ++;
+window["w" + tmp] = "";
+window["b" + tmp] = "";
+window["y" + tmp] = 54;
+window["n" + tmp] = 46;
+
+
+
+fileLoaded = true; //do not remove this
